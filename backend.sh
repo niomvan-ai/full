9a7791ab3fae2f78b2cfbae344e167f01af3173c
albedon <127307786+albedon@users.noreply.github.com>
2025-01-24 14:19:42 +0400

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -1,6 +1,8 @@
 cd backend
 echo "Starting Django backend server..."
 python.exe -m pip install --upgrade pip
+git lfs install
+git lfs pull
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
