b2a6d0b4038ef692ea53c9d47efec9359ed8f9c9
albedon <127307786+albedon@users.noreply.github.com>
2025-01-24 15:19:09 +0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
-
+const apiUrl = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api/';
 const api = axios.create({
-  baseURL: "https://cautious-space-garbanzo-pxrrvjv9g67hvg6-8000.app.github.dev/api/",
+  baseURL: apiUrl,
 });
 
 api.interceptors.request.use(
